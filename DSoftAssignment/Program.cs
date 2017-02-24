78b5255de4bfd4c0166bafb3b225e6a35c999187
LinSamuel <samueltanlin@gmail.com>
2017-02-23 21:45:18 -0800

    added file reading

--- DSoftAssignment/Program.cs
+++ DSoftAssignment/Program.cs
@@ -10,6 +10,11 @@ namespace DSoftAssignment
     {
         static void Main(string[] args)
         {
+            string text = System.IO.File.ReadAllText(@"C:\Users\sam\Documents\Visual Studio 2013\Projects\DSoftAssignment\input.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            Console.ReadKey();
         }
     }
 }
